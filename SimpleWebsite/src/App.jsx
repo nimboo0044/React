bfd364445a09778418f130726bad38aed6707ce1
nimboo0044 <nimazr800@gmail.com>
2023-09-06 12:36:27 +0330

    editted App.jsx

--- SimpleWebsite/src/App.jsx
+++ SimpleWebsite/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Mypage from '../../files/mypage'
+import Header from '../../files/Header'
 
 function App() {
 
   return (
     <>
+      <Header />
       <Mypage />
     </>
   )
